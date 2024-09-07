066b5b236fbcd1c87ce02473d39d91bcd5b0290e
dpetzer <davepetzer@yahoo.com>
2024-09-07 12:09:04 +1000

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+date
+if mount -v | grep /media/external1 > /dev/null;
+then 
+        echo "External 1 is mounted"
+else
+        echo "External 1 is not mounted - nothing to copy"
+        exit
+fi 
+
+if mount -v | grep /media/external2 > /dev/null;
+then 
+        echo "External 2 is mounted so copy to external 2"
+        sudo rsync -av --delete /media/external1/ /media/external2
+else
+        echo "External 2 is not mounted - copy from ext 2"
+fi 
+
+if mount -v | grep /media/external3 > /dev/null;
+then 
+        echo "External 3 is mounted so copy to external 3"
+        sudo rsync -av --delete /media/external1/ /media/external3
+else
+        echo "External 3 is not mounted"
+fi
+
